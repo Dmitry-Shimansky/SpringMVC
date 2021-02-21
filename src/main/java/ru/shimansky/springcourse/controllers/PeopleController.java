4669ee6441949163ec94b2c932cf459466d362dc
ext_dmitrys <ext_dmitrys@playtika.com>
2021-02-21 21:02:26 +0300

    Lesson 22 - Annotation @ModelAttribute

--- src/main/java/ru/shimansky/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/shimansky/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.shimansky.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.shimansky.springcourse.dao.PersonDAO;
+import ru.shimansky.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -33,5 +32,18 @@ public class PeopleController {
 
         model.addAttribute("person", personDAO.show(id));
         return "poeple/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
